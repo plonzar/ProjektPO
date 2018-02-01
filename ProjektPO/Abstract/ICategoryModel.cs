743153cfd7f6ff0f358cb235c40617f8211d41d6
grzmigrot <rezzi00@gmail.com>
2018-02-01 01:39:06 +0100

    categorymodel od piotrka

--- ProjektPO/Abstract/ICategoryModel.cs
+++ ProjektPO/Abstract/ICategoryModel.cs
@@ -1,4 +1,5 @@
 ﻿using ProjektPO.Entity;
+using ProjektPO.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,13 @@ namespace ProjektPO.Abstract
 {
     public interface ICategoryModel
     {
-        void AddCategory();
-        void AddCategoryItem();
-        void DeleteCategory();
-        void DeleteCategoryItem();
-        void EditCategory();
-        void ReadCategory();
-        void ReadCategoryItems(CategoryEntity category);
+        bool AddCategory(CategoryViewModel categoryViewModel, int userId);        
+        bool DeleteCategory(int categoryId, int userId);
+        bool EditCategory(CategoryViewModel categoryViewModel, int userId);
+        CategoryViewModel ReadCategory(string categoryName, int userId);
+        bool DeleteCategoryItem(int categoryId, int userId);     
+        bool AddCategoryItem(CategoryItemViewModel categoryItemViewModel, int categoryId,int userId);
+        List<CategoryItemViewModel> ReadCategoryItems(int categoryId);
 
     }
 }
